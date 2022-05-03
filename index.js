8ef9f7bbd83ef1b99418eff4889ba3537b4c22f0
Dyzzal <85768713+Dyzzal@users.noreply.github.com>
2022-05-02 17:31:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-function processUser()
-{
+
     var parameters = location.search.substring(1).split("&");
 
     var temp = parameters[0].split("=");
     l = unescape(temp[1]);
     temp = parameters[1].split("=");
     p = unescape(temp[1]);
-    document.getElementById("hi").innerHTML = l;
-}
+    document.getElementById("log").innerHTML = l;
+    document.getElementById("pass").innerHTML = p;
